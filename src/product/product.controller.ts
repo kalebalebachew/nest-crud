65810c3b46eb2572cad948ddf9adcf8135eddd6e
Kaleb <Kalebalebachew4@gmail.com>
2024-11-10 18:18:17 +0300

    Code Diff Summary: - The .prettierrc and package.json files were updated to change a dependency version. - AppService.getHello() was updated to return a different message. - ProductController had some refactoring, it is no longer returning the placeholder strings and has new functionality for creating a new product in the list, updating, deleting and getting a product by id. - The ProductController was refactored, it no longer returns the placeholder strings 'all products', 'updated product ', ' deleted product', 'get one product' and 'create product' in the function bodies but it has new functionality for creating a new product in the list, getting a product by id, update and delete based on the id, and changing the routing Redirect in @Get(). - The application service was refactored to return a new message. - The ProductService and createProductDto modules have been introduced.
    Code Change Type: refactor
    Commit Message: Refactor ProductController, AppService and update config files

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,45 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  Redirect,
+  HttpCode,
+} from '@nestjs/common';
+import { createProductDto } from './dto/create-product.dto';
 
 @Controller('product')
 export class ProductController {
   @Get()
-  findAll(): string {
-    return 'all products';
+  @HttpCode(302)
+  @Redirect('https://hudc.org')
+  getAllProducts(): string {
+    return 'all products returned';
+  }
+
+  @Post('create')
+  createProduct(@Body() create: createProductDto): string {
+    return `name: ${create.name} Description: ${create.description}`;
+  }
+
+  @Get(':id')
+  getOneProduct(@Param('id') id: string): string {
+    return `id: ${id}`;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string): string {
+    return `Deleted Product: ${id}`;
+  }
+
+  @Put(':id')
+  updateProduct(
+    @Body() update: createProductDto,
+    @Param('id') id: string,
+  ): string {
+    return `Updated Product: ${update.name}, ${update.description}`;
   }
 }

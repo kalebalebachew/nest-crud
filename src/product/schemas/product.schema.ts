12bc7deead706911e75fbd423c3a4374c1ba7712
Kaleb <Kalebalebachew4@gmail.com>
2024-11-10 21:04:21 +0300

    config/keys file to read the mongo credentials from enviroment variables

---
+++ src/product/schemas/product.schema.ts
@@ -0,0 +1,6 @@
+import * as mongoose from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
